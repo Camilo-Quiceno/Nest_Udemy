fb848f3b6d11eb745cf84f9e0e0e2bce0f401580
Camilo-Quiceno <camilo.quiceno.q@gmail.com>
2024-03-03 16:59:18 -0500

    S02E13

--- typescript-intro/src/bases/03-classes.ts
+++ typescript-intro/src/bases/03-classes.ts
@@ -1,8 +1,26 @@
 export class Pokemon {
+
+    get imageUrl():string {
+        return `https:://pokemon.com/${this.id}.jpg`
+    }
+
     constructor(
         public readonly id: number, 
-        public name: string
+        public name: string,
     ) {} 
+
+    scream(){
+        console.log(`${this.name.toUpperCase()}!!`)
+    }
+
+    speak(){
+        console.log(`I'm ${this.name}!!`)
+    }
 }
 
-export const pikachu = new Pokemon( 4, 'pikachu')
+export const pikachu = new Pokemon( 4, 'pikachu');
+
+console.log(pikachu);
+
+pikachu.scream();
+pikachu.speak();
