2233a1535b68b261f098061d00de8b49cf80b52b
Camilo-Quiceno <camilo.quiceno.q@gmail.com>
2024-03-04 06:44:01 -0500

    S02E19

--- typescript-intro/src/main.ts
+++ typescript-intro/src/main.ts
@@ -1,11 +1,13 @@
-import { pikachu } from './bases/03-classes';
+//import { pikachu } from './bases/03-classes';
+import { charmander } from './bases/04-inyection';
+
 
 import './style.css'
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = `
-                <h1>Hello ${pikachu.name}!!</h1>
+                <h1>Hello ${charmander.name}!!</h1>
                 `;
 
 
