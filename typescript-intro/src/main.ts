7882867a07e0bbb8669f269a4eb4c70e79a978de
Camilo-Quiceno <camilo.quiceno.q@gmail.com>
2024-03-04 07:34:46 -0500

    S02-Completed

--- typescript-intro/src/main.ts
+++ typescript-intro/src/main.ts
@@ -1,6 +1,6 @@
 //import { pikachu } from './bases/03-classes';
-import { charmander } from './bases/04-inyection';
-
+//import { charmander } from './bases/04-inyection';
+import { charmander } from './bases/06-decorators2';
 
 import './style.css'
 
@@ -11,3 +11,4 @@ app.innerHTML = `
                 `;
 
 
+            
