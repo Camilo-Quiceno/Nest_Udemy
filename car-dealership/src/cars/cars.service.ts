3a95c1ec7cc9828d0b4d711d8c2f5c11d119959f
Camilo-Quiceno <camilo.quiceno.q@gmail.com>
2024-03-06 09:53:12 -0500

    Addi cardealear to the repo

---
+++ car-dealership/src/cars/cars.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+  private cars = [
+    {
+      id: 1,
+      brand: 'Toyota',
+      model: 'Corolla',
+    },
+    {
+      id: 2,
+      brand: 'BMW',
+      model: 'X5',
+    },
+    {
+      id: 3,
+      brand: 'Renault',
+      model: 'Clio',
+    },
+  ];
+
+  findAll() {
+    return this.cars;
+  }
+
+  findOneById(id: number) {
+    const car = this.cars.find((car) => car.id === id);
+    if (!car) {
+      throw new NotFoundException(`Car with id ${id} not found`);
+    }
+
+    return car;
+  }
+}
