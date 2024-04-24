daf4c749da7c4fc35ddac2e1de1bd17f2ff0c740
Camilo-Quiceno <camilo.quiceno.q@gmail.com>
2024-04-24 09:18:45 -0500

    S08E95

--- pokedex/src/seed/seed.service.ts
+++ pokedex/src/seed/seed.service.ts
@@ -1,21 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name) private pokemonModel: Model<Pokemon>,
+  ) {}
+
   async executeSeed() {
+    this.pokemonModel.deleteMany({});
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=650',
+      'https://pokeapi.co/api/v2/pokemon?limit=100',
     );
 
-    data.results.forEach(({ name, url }) => {
+    const pokemons = data.results.map(({ name, url }) => {
       const segments = url.split('/');
-      const no:number = +segments[segments.length - 2];
-      console.log({ no, name });
+      const no: number = +segments[segments.length - 2];
+      return { no, name };
     });
+    await this.pokemonModel.insertMany(pokemons);
     return data.results;
   }
 }
